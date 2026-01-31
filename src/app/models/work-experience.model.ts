d6469311f7d3488b76ce378fbf1200560dcf9d54
SasiVardhanReddy3 <SasiVardhanReddy3@users.noreply.github.com>
2026-01-31 13:44:02 +0000

    deploy: dcb7727999339549ef68ade74a940153780cf431

--- src/app/models/work-experience.model.ts
+++
@@ -1,8 +0,0 @@
-export interface WorkExperience {
-    position: string;
-    company: string;
-    time: string;
-    location: string;
-    description: string;
-    responsibilities: string[];
-  }
